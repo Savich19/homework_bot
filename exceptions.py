c34975108daa1aac19129d6de33de309b7b71873
Александр Савич <rcdrifts@ya.ru>
2022-09-02 03:01:36 +0300

    First commit 2.0

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,25 @@ class NotTwoHundred(Exception):
 
 class NotList(Exception):
     pass
+
+
+class NotSend(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'NotSend: {0} '.format(self.message)
+        else:
+            return 'Сработала ошибка NotSend'
+
+
+class NotResponse(Exception):
+    pass
+
+
+class NotStatus(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -7,8 +7,11 @@ import time
 from dotenv import load_dotenv
 from http import HTTPStatus
 from exceptions import (
-    NotTwoHundred,
     NotList,
+    NotResponse,
+    NotSend,
+    NotStatus,
+    NotTwoHundred,
 )
 
 
@@ -22,17 +25,11 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='program.log',
-    format='%(asctime)s, %(levelname)s, %(message)s'
-)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -42,18 +39,27 @@ logger.addHandler(handler)
 
 def send_message(bot, message):
     """Отправка сообщения в Telegram чат."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logger.info('Бот начал отправку сообщения')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except NotSend:
+        message = 'Бот не смог отправить сообщение'
+        logger.error(message)
 
 
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту API-сервиса Практикума."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(
-        url=ENDPOINT,
-        headers=HEADERS,
-        params=params
-    )
+    try:
+        response = requests.get(
+            url=ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+    except NotResponse:
+        message = 'Не удалось получить ответ от Практикума'
+        logger.error(message)
     if response.status_code != HTTPStatus.OK:
         raise NotTwoHundred('Ошибка соединения')
     return response.json()
@@ -62,7 +68,7 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверка ответа API на корректность."""
     if not isinstance(response['homeworks'], list):
-        raise NotList
+        raise NotList('Формат ответа не соответствует ожидаемому')
     return response['homeworks']
 
 
@@ -70,7 +76,9 @@ def parse_status(homework):
     """Получение статуса конкретной домашки."""
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    verdict = HOMEWORK_STATUSES[homework_status]
+    if not homework_status in HOMEWORK_VERDICTS.keys():
+        raise NotStatus('Статус домашки не известен')
+    verdict = HOMEWORK_VERDICTS[homework_status]
     message = (
         f'Изменился статус проверки работы '
         f'"{homework_name}". {verdict}'
@@ -81,7 +89,7 @@ def parse_status(homework):
 
 def check_tokens():
     """Проверка переменных окружения."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
         message = 'Переменные окружения найдены!'
         logger.info(message)
         return True
@@ -92,6 +100,9 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
+    if not check_tokens():
+        sys.exit('Переменные окружения не найдены')
+
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
@@ -101,39 +112,47 @@ def main():
     last_homeworks = {}
 
     while True:
-        if check_tokens():
-            try:
-                response = get_api_answer(current_timestamp)
-                logger.info('Запрос к API-сервису прошел успешно')
-                homeworks = check_response(response)
-                logger.info('Ответ от API-сервиса корректен')
-                if not homeworks:
-                    logger.info('Новых домашек нет. Сообщение не отправлено')
-                else:
-                    for homework in homeworks:
-                        message = parse_status(homework)
-                        homework_name = homework['homework_name']
-                        if homework_name in last_homeworks.keys():
-                            new_status = homework['status']
-                            last_status = last_homeworks[homework_name]
-                            if new_status != last_status:
-                                send_message(bot, message)
-                                logger.info(
-                                    'Статус дз изменен. Сообщение отправлено')
-                        else:
+        try:
+            response = get_api_answer(current_timestamp)
+            logger.info('Запрос к API-сервису прошел успешно')
+            homeworks = check_response(response)
+            logger.info('Ответ от API-сервиса корректен')
+            if not homeworks:
+                logger.info('Новых домашек нет. Сообщение не отправлено')
+            else:
+                for homework in homeworks:
+                    message = parse_status(homework)
+                    homework_name = homework['homework_name']
+                    if homework_name in last_homeworks.keys():
+                        new_status = homework['status']
+                        last_status = last_homeworks[homework_name]
+                        if new_status != last_status:
                             send_message(bot, message)
                             logger.info(
-                                'Найдена новое дз. Сообщение отправлено!')
-                        last_homeworks[homework_name] = homework['status']
-                    current_timestamp = response.get(
-                        'current_date', current_timestamp)
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
-                logger.error(message)
-                send_message(bot, message)
-            finally:
-                time.sleep(RETRY_TIME)
+                                'Статус дз изменен. Сообщение отправлено')
+                    else:
+                        send_message(bot, message)
+                        logger.info(
+                            'Найдена новое дз. Сообщение отправлено!')
+                    last_homeworks[homework_name] = homework['status']
+                current_timestamp = response.get(
+                    'current_date', current_timestamp)
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            logger.error(message)
+            send_message(bot, message)
+        finally:
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='program.log',
+        filemode='w',
+        format=" ".join([
+            '%(asctime)s: %(levelname)s -> %(message)s',
+            '[%(filename)s : %(funcName)s : %(lineno)s]'
+        ])
+    )
     main()
